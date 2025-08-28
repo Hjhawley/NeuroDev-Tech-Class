a68ae5cc087adc8ba32169c0334082d6e3fa1091
Hayden <hjhawley7@gmail.com>
2025-08-28 13:16:47 -0600

    load head and header dynamically

---
+++ assets/js/load-head.js
@@ -0,0 +1,23 @@
+document.head.innerHTML += `
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta name="robots" content="index, follow" />
+  <meta
+    name="description"
+    content="Explore NeuroDev Tech Class courses including Python, web development, game design, and tech skills in St. George. Our curriculum focuses on practical, hands-on learning for neurodivergent students."
+  />
+  <meta
+    name="keywords"
+    content="NeuroDev Tech Class, vocational tech school St. George, tech curricula NeuroDev, Python courses, web development courses, game development courses, St. George UT, neurodivergent tech education, neurodevelopmental mentoring, NDM"
+  />
+  <meta name="author" content="NeuroDev" />
+  <title>NeuroDev Tech Class - Course Catalog</title>
+  <link rel="stylesheet" href="/site/assets/css/styles.css" />
+  <link
+    rel="icon"
+    type="image/png"
+    sizes="32x32"
+    href="/site/assets/images/NeuroDev-favicon-32x32.png"
+  />
+  <base id="base" href="/site/" />
+  `;
---
+++ assets/js/load-header.js
@@ -0,0 +1,12 @@
+document.querySelector("header").innerHTML = `
+  <img
+    src="assets/images/logo-white.png"
+    alt="NeuroDev Logo"
+    class="logo"
+  />
+  <nav>
+    <a href="index.html">Home</a>
+    <a href="catalog.html">Course Catalog</a>
+    <a href="resources.html">Resources</a>
+  </nav>
+`;
